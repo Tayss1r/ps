762f59a59a28c5f7567c0c7bd943c19c01f959ad
unknown <ferhitayssir@gmail.com>
2025-03-17 23:26:17 +0100

    purr

---
+++ cppcf/B_Equal_Candies.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+#define all(t) t.begin(),t.end()
+#define rev(t) reverse(t.begin(), t.end())
+#define ln '\n'
+using namespace std;
+
+void solve() {
+    int n;
+    cin >> n;
+    vector<int> v;
+    for (int i = 0; i < n; i++)
+    {
+        int a;
+        cin >> a;
+        v.push_back(a);    
+    }
+
+    int min = *min_element(all(v));
+    int ans = 0;
+    for (int i = 0; i < n; i++)
+    {
+        ans += v[i] - min;
+    }
+    
+    cout << ans << ln;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
