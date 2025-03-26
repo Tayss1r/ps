86c99e1d9b02b7350fa19bc4d80ee3a685dd6dde
unknown <ferhitayssir@gmail.com>
2025-03-26 06:48:30 +0100

    mms

---
+++ cppcf/A_Collecting_Coins.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#define all(t) t.begin(),t.end()
+#define rev(t) reverse(t.begin(), t.end())
+#define ln '\n'
+using namespace std;
+
+void solve() {
+    
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
---
+++ cppcf/B_Maximum_Multiple_Sum.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#define all(t) t.begin(),t.end()
+#define rev(t) reverse(t.begin(), t.end())
+#define ln '\n'
+using namespace std;
+
+void solve() {
+    int n;
+    cin >> n;
+    cout << (n==3?3:2) <<ln;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
