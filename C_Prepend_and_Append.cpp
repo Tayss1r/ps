c1de95fa1397c1c5361623a89b05759296cdd2ef
Tayssir Ferhi <ferhitayssir@gmail.com>
2025-05-08 23:44:46 +0100

    prepend and append

---
+++ C_Prepend_and_Append.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#define all(t) t.begin(), t.end()
+#define rev(t) reverse(t.begin(), t.end())
+#define ln '\n'
+using namespace std;
+
+void solve() {
+    int n;
+    string s;
+    cin >> n >> s;
+    int l = 0, r = n - 1;
+    while (l < r && s[l] != s[r]) {
+        l++;
+        r--;
+    }
+    cout << r - l + 1 << '\n';
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
